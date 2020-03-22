1197cf1cbdf924db58a738531c149585b2aeaec7
huhn511 <sehe89@gmail.com>
2020-03-22 19:05:50 +0100

    try to add new assetsPublicPath

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'marketplace',
     proxyTable: {},
 
     // Various Dev Server settings
