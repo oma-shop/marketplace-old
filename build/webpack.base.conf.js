14e219a45a72ee59e583825fd2495d83ea12996f
huhn511 <sehe89@gmail.com>
2020-03-22 19:09:50 +0100

    change path

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -29,9 +29,7 @@ module.exports = {
     path: config.build.assetsRoot,
     filename: '[name].js',
     publicPath:
-      process.env.NODE_ENV === 'production'
-        ? config.build.assetsPublicPath
-        : config.dev.assetsPublicPath
+      process.env.NODE_ENV === '/marketplace/'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
